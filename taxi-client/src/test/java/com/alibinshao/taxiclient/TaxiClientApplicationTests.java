6d284238a12562aa62a6e4c54471d05468be41f3
alibinshao <liangbinbin@aliyun.com>
2024-05-14 09:36:08 +0800

    taxi

---
+++ taxi-client/src/test/java/com/alibinshao/taxiclient/TaxiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibinshao.taxiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
